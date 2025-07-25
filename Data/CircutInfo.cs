ecb8419f5cb8ca186f57eb025d7ac5a8c40d249d
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2025-07-25 20:31:37 +0200

    Initial commit

---
+++ Data/CircutInfo.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper;
+
+namespace F1StrategySite.Data
+{
+    public static class CircutInfo
+    {
+        private const string FilePath = @"Data\CircutInfo.cs";
+        public ReadFile(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Circut>().ToList();
+                // Process records as needed
+            }
+        }
+    }
+}
