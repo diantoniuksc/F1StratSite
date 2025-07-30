44305475b9e3d4d11acf172e202ac391a5168e1b
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2025-07-30 21:23:10 +0200

    Created main branding for the website

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +23,5 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
+app.MapControllers();
 app.Run();
