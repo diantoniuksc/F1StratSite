0464054496914a265a05fbb25c95e98d34848ce4
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2025-08-13 15:40:18 +0200

    Added ML tyre tyre life prediction

--- F1StrategySite/Data/Strategies.cs
+++ F1StrategySite/Data/Strategies.cs
@@ -1,4 +1,5 @@
-﻿using CsvHelper;
+﻿using F1StartegySite.MLModel;
+using System.IO;
 
 namespace F1StrategySite.Data
 {
@@ -6,8 +7,9 @@ namespace F1StrategySite.Data
     {
         public string GPName { get; set; } = name;
         public string Strategy { get; set; }
-        public int StrategyFrequency { get; set; }
-        private string FilePath { get; set; } = path;
+        public int StrategyFrequencyInt { get; set; }
+        public string StrategyFrequencyText { get; set; }
+        public string FilePath { get; set; } = path;
         private static string StrategiesData { get; set; }
 
 
@@ -32,13 +34,66 @@ namespace F1StrategySite.Data
                     {
                         Strategies stratRow = new(GPName);
                         stratRow.Strategy = gpEntry[1];
-                        stratRow.StrategyFrequency = Convert.ToInt32(gpEntry[2]);
+                        stratRow.StrategyFrequencyInt = Convert.ToInt32(gpEntry[2]);
+                        stratRow.StrategyFrequencyText = MapFrequecyIntStr(stratRow.StrategyFrequencyInt);
                         return stratRow;
                     }
                     return null;
                 }).ToArray();
 
             return strategiesArr.Where(row => row != null).ToArray();
+        }
+
+        private static string MapFrequecyIntStr(int numFrq)
+        {
+            return numFrq switch
+            {
+                > 0 and <= 10 => "Low Frequency",
+                > 10 and <= 20 => "Medium Frequency",
+                > 20 => "High Frequency",
+                _ => "Unknown"
+            };
+        }
+
+        public async Task<float> GetTyreLifeAsync(string tyreType, string driver_id, int start_lap)
+        {
+            float circuitLength = await CircutInfo.GetCircuitLengthAsync(GPName);
+
+            MLModel1.ModelInput input = new()
+            {
+                Driver_id = driver_id,
+                Race_length = circuitLength,
+                Year = 2025,
+                Compound = tyreType,
+                Stint_start_lap = start_lap
+            };
+
+            var prediction = MLModel1.Predict(input);
+            return prediction.Score;
+        }
+
+        public async Task<float[]> GetStrategyStintsAsync()
+        {
+            if(Strategy == null || Strategy.Length == 0)
+                throw new InvalidOperationException("Strategy is not set or empty.");
+
+            float[] stintLife = new float[Strategy.Length];
+
+
+            for(int i = 0; i < Strategy.Length; i++)
+            {
+                string tyreType = Strategy[i] switch
+                {
+                    'S' => "SOFT",
+                    'M' => "MEDIUM",
+                    'H' => "HARD",
+                    _ => throw new ArgumentException("Invalid tyre type")
+                };
+
+                stintLife[i] = await GetTyreLifeAsync(tyreType, "VER", 1);
+            }
+
+            return stintLife;
         }
     }
 }
