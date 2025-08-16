a8c5b1315a327008dbbece3004551238a21ffee9
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2025-08-16 20:54:02 +0200

    Added tests for Key race Statistics

---
+++ F1Test/TestPitstops.cs
@@ -0,0 +1,25 @@
+﻿using F1StrategySite.Data;
+
+namespace F1Test
+{
+    [TestClass]
+    public sealed class TestPitstops
+    {
+        [TestMethod]
+        public async Task TestPitstopsApi()
+        {
+            Pitstops pitstops = new(11, 2024);
+            int count = await pitstops.GetTotalAsync();
+            float longest = await pitstops.GetLongestAsync();
+            float shortest = await pitstops.GetFastestAsync();
+
+
+            int expectedCount = 30;
+            float expectedLongest = 33.947f;
+            float expectedShortest = 20.978f;
+
+            Assert.AreEqual((expectedCount, expectedLongest, expectedShortest), (count, longest, shortest));
+            
+        }
+    }
+}
