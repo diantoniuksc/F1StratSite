1ae09fa997c764e5b7718ade431c211a3b74011f
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2025-08-20 00:14:15 +0200

    StrategyHome now contains real schedule

---
+++ F1StrategySite/Data/RaceInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace F1StrategySite.Data
+{
+    public class RaceInfo
+    {
+        public string GrandPrix { get; set; }
+        public string Circuit { get; set; }
+        public int Round { get; set; }
+    }
+}
