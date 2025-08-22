95a4b4c03fd231d162141c24089331a604f1d9e6
diantoniuksc <162093949+diantoniuksc@users.noreply.github.com>
2025-08-23 00:30:09 +0200

    Prepare for production. Custom error page

--- F1StrategySite/Program.cs
+++ F1StrategySite/Program.cs
@@ -5,10 +5,18 @@ using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Ensure static web assets (including CSS isolation) are served when running from source in any environment
+builder.WebHost.UseStaticWebAssets();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddControllers();
+builder.Services.AddRazorPages(options =>
+{
+    // Razor Pages live under Components/Pages
+    options.RootDirectory = "/Components/Pages";
+});
 builder.Services.AddRateLimiter(options =>
 {
     options.AddFixedWindowLimiter("PredictionLimiter", limiterOptions =>
@@ -25,6 +33,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseStatusCodePagesWithReExecute("/Error", "?code={0}");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -37,5 +46,6 @@ app.UseRateLimiter();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+app.MapRazorPages();
 app.MapControllers();
 app.Run();
